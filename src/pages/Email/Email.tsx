0e8a0a55a74239a91010a5d91456160a72e72469
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-19 17:50:24 -0600

    feat(App): handle form data

--- src/pages/Email/Email.tsx
+++ src/pages/Email/Email.tsx
@@ -1,17 +1,30 @@
-import { useForm, useFormState } from "react-hook-form";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { IUserInfoContext } from "../../layout/types";
+import { Context } from "../../layout/useLayoutContext";
 
 import { Input } from "../../UI-components/input";
 
 export const Email = () => {
-  const { register, handleSubmit, setValue } = useForm();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<{ email: string }>();
+  const { userInformation, setUserInformation } = useContext(
+    Context
+  ) as IUserInfoContext;
 
   const formInputProps = {
     register,
     required: true,
   };
 
+  const onSubmit = ({ email }: { email: string }) => {
+    setUserInformation({ ...userInformation, email });
+    navigate("/address");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Input
         name="email"
         placeholder="Escribe tu correo electrónico"
