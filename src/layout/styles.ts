dd67a4c04188fc292787234fb22f1a4ccdb5f6c6
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-19 09:05:42 -0600

    feat(App): add initial layout with grid css

---
+++ src/layout/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  display: grid;
+  grid-gap: 10px;
+
+  height: 100vh;
+
+  grid-template-areas:
+    "header header header"
+    "content content content"
+    "footer footer footer";
+
+  grid-template-columns: 1fr 5fr 2fr;
+  grid-template-rows: 1fr 5fr 2fr;
+
+  @media (min-width: 600px) {
+    grid-template-areas:
+      "header header leftPanel"
+      "content content leftPanel"
+      "footer footer leftPanel";
+
+    grid-template-columns: 1fr 6fr 4fr;
+    grid-template-rows: 1fr 5fr 1fr;
+  }
+`;
+
+export const Content = styled.article`
+  grid-area: content;
+`;
+
+export const Header = styled.header`
+  grid-area: header;
+`;
+export const LeftPanel = styled.aside`
+  grid-area: leftPanel;
+
+  display: none;
+
+  @media (min-width: 600px) {
+    display: block;
+  }
+`;
+export const Footer = styled.footer`
+  grid-area: footer;
+`;
