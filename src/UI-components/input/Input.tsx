6d1242fa855bc651d788cebc8d577cc6d6b5d909
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-19 14:13:50 -0600

    feat(App): update types in FormInput

--- src/UI-components/input/Input.tsx
+++ src/UI-components/input/Input.tsx
@@ -1,20 +1,51 @@
-import { Box, FormInput, Label } from "./styles";
+import { ChangeEvent, useState } from "react";
+import { UseFormRegister } from "react-hook-form";
+
+import { Asterisk, Box, ErrorMessage, Flex, FormInput, Label } from "./styles";
+
+interface IFormInput {
+  label: string;
+  type: string;
+  handleOnChange: () => void;
+  placeholder: string;
+  name: string;
+  register?: UseFormRegister<any> | null;
+  defaultValue?: string;
+  required?: boolean;
+}
 
 export const Input = ({
   label,
   type,
-  value,
   handleOnChange,
-}: {
-  label: string;
-  type: string;
-  value: string;
-  handleOnChange: () => {};
-}) => {
+  placeholder,
+  name,
+  register = null,
+  defaultValue = "",
+  required = false,
+}: IFormInput) => {
+  const [value, setValue] = useState(defaultValue);
+  console.log(value, "value");
+
   return (
     <Box>
-      <Label>{label}</Label>
-      <FormInput type={type} onChange={handleOnChange} />
+      <Label>
+        {label} {required && <Asterisk>*</Asterisk>}
+      </Label>
+      <Flex>
+        <FormInput
+          placeholder={placeholder}
+          value={value}
+          type={type}
+          {...(register &&
+            register(name, {
+              required: { value: required, message: "Campo requerido" },
+              onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                setValue(e.target.value),
+            }))}
+        />
+        {false && <ErrorMessage>Error</ErrorMessage>}
+      </Flex>
     </Box>
   );
 };
