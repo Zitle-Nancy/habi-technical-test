e2fa95ed27ee590c55e546dcc3045a608cc56c8d
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-19 14:16:08 -0600

    feat(App): send the required props in Input component

--- src/pages/UserData/UserData.tsx
+++ src/pages/UserData/UserData.tsx
@@ -1,14 +1,26 @@
+import { useForm, useFormState } from "react-hook-form";
+
 import { Input } from "../../UI-components/input";
 
 export const UserData = () => {
+  const { register, handleSubmit, setValue } = useForm();
+
+  const formInputProps = {
+    register,
+    required: true,
+  };
+
   return (
     <form>
       <Input
+        name="userName"
+        placeholder="Nombre completo"
         type="text"
-        value=""
         label="Nombre completo"
         handleOnChange={() => ""}
-      ></Input>
+        {...formInputProps}
+      />
+      <button type="submit">next</button>
     </form>
   );
 };
