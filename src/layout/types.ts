cc310319853ae2379a3404d2049523ba0667d1c1
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-20 06:34:21 -0600

    feat(App): add IAmenities interface

--- src/layout/types.ts
+++ src/layout/types.ts
@@ -1,8 +1,13 @@
+// TODO add the amenities values
+export interface IAmenities {
+  [key: string]: boolean | string;
+}
 export interface IUserInformation {
   fullName: string;
   email: string;
   address: string;
   floorNumber: string;
+  amenities: IAmenities[];
 }
 
 export interface IUserInfoContext {
