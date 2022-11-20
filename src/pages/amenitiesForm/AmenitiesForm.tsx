134eeb9073b9cba62907e1fa3d03f7a3ce399086
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-20 00:32:08 -0600

    feat(App): create AmenitiesForm page

---
+++ src/pages/amenitiesForm/AmenitiesForm.tsx
@@ -0,0 +1,5 @@
+import { Checkbox } from "../../UI-components/checkbox";
+
+export const AmenitiesForm = () => {
+  return <Checkbox textLabel="holis" />;
+};
---
+++ src/pages/amenitiesForm/index.ts
@@ -0,0 +1,1 @@
+export { AmenitiesForm } from "./AmenitiesForm";
