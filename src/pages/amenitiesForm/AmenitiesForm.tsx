ee7c0fe2310a6b36f74ac546cd23c3633644cdf4
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-21 00:00:10 -0600

    feat(App): add styles in AmenitiesForm

--- src/pages/amenitiesForm/AmenitiesForm.tsx
+++ src/pages/amenitiesForm/AmenitiesForm.tsx
@@ -3,9 +3,11 @@ import { useForm } from "react-hook-form";
 
 import { IUserInfoContext } from "../../layout/types";
 import { Context } from "../../layout/useLayoutContext";
+import { Typography } from "../../UI-components";
 import { Button } from "../../UI-components/button";
 import { Checkbox } from "../../UI-components/checkbox";
 import { AMENITIES_OPTIONS } from "./constants";
+import { Flex, Wrapper } from "./styles";
 import { IAmenitiesOptions } from "./types";
 
 export const AmenitiesForm = () => {
@@ -20,23 +22,34 @@ export const AmenitiesForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>
-        Por favor, selecciona si el apartamento tiene alguna de las siguientes
-        opciones:
-      </h1>
-      {AMENITIES_OPTIONS.map((amenity: IAmenitiesOptions, index: number) => {
-        return (
-          <Checkbox
-            register={register}
-            name={AMENITIES_OPTIONS[index].id}
-            registerName={AMENITIES_OPTIONS[index].name}
-            key={amenity.name}
-            textLabel={amenity.name}
-          />
-        );
-      })}
-      <Button type="submit" text="Ver resumen →" />
+    <form onSubmit={handleSubmit(onSubmit)} style={{ margin: "1rem 5rem" }}>
+      <Typography
+        display="block"
+        margin="0 0 0.75rem 0"
+        fontWeight="400"
+        fontSize="20px"
+        color="#252129"
+      >
+        Selecciona si el departamento tiene alguna de las siguientes opciones:
+      </Typography>
+      <Wrapper>
+        <Flex>
+          {AMENITIES_OPTIONS.map(
+            (amenity: IAmenitiesOptions, index: number) => {
+              return (
+                <Checkbox
+                  register={register}
+                  name={AMENITIES_OPTIONS[index].id}
+                  registerName={AMENITIES_OPTIONS[index].name}
+                  key={amenity.name}
+                  textLabel={amenity.name}
+                />
+              );
+            }
+          )}
+        </Flex>
+        <Button type="submit" text="Ver resumen →" borderRadius="8px" />
+      </Wrapper>
     </form>
   );
 };
