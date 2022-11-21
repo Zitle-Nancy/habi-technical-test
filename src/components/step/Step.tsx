43f06a2590d83fc6c10a3d38efd99100e29d4833
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-20 23:59:11 -0600

    chore(App): refactor copy and styles

--- src/components/step/Step.tsx
+++ src/components/step/Step.tsx
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom";
+
+import { Typography } from "../../UI-components";
 import { STEPS_JSON } from "./stepsJSON";
+import { Wrapper } from "./styles";
 import { IPaths } from "./types";
 
 export const Step = () => {
@@ -9,8 +12,27 @@ export const Step = () => {
   const PATHS_NUMBER_TOTALS = Object.keys(STEPS_JSON).length;
 
   return (
-    <div>
-      Paso {CURRENT_PATH?.order} de {PATHS_NUMBER_TOTALS}: {CURRENT_PATH?.title}{" "}
-    </div>
+    <>
+      {pathname === "/" ? (
+        <Typography fontSize="24px">
+          Hola, estas a punto de comenzar tu proceso con nosotros.
+        </Typography>
+      ) : (
+        <Wrapper>
+          <Typography color="#6301cc" fontSize="32px" fontWeight="600">
+            Paso {CURRENT_PATH?.order} de {PATHS_NUMBER_TOTALS}:{" "}
+            {CURRENT_PATH?.title}{" "}
+          </Typography>
+          <Typography
+            color="#6d6970"
+            fontSize="16px"
+            display="block"
+            margin="18px 0 0 0"
+          >
+            {CURRENT_PATH?.description}
+          </Typography>
+        </Wrapper>
+      )}
+    </>
   );
 };
