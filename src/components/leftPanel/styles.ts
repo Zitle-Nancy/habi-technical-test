e262afce13949aa012c3a3d279beeff76f8c72cc
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-11-21 00:36:18 -0600

    feat(App): add styles in LeftPannel

--- src/components/leftPanel/styles.ts
+++ src/components/leftPanel/styles.ts
@@ -3,6 +3,16 @@ import styled from "styled-components";
 export const LeftPanel = styled.aside`
   grid-area: leftPanel;
 
+  padding: 80px 28px 0px 24px;
+  display: block;
+  grid-column: 10 / span 2;
+  width: 340px;
+  height: 100vh;
+  background: #fcfcfc;
+  position: fixed;
+  top: 0px;
+  right: 0px;
+
   display: none;
 
   @media (min-width: 600px) {
